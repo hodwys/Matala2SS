1f51b6c6421162a9ddd59d0899fa8f3db6a3e69f
hodwys <hodwys2424@gmail.com>
2023-03-22 12:36:37 +0200

    test1

--- test.cpp
+++ test.cpp
@@ -13,9 +13,44 @@ TEST_CASE("BEFOR START GAME"){
 
 Player One("One"); //Creating player objects
 Player Two("Two");
-Game(One, Two);
+Game game(One, Two);
 CHECK(One.stacksize() == Two.stacksize && One.stacksize==26);
+CHECK(One.cardesTaken==0 && Two.cardesTaken==0);
 }
 
 
+
+TEST_CASE("END THE GAME"){
+
+Player One("One"); //Creating player objects
+Player Two("Two");
+Game game(One, Two);
+game.playAll();
+CHECK((One.stacksize() == 0 && Two.stacksize==52) ||(One.stacksize() == 52 && Two.stacksize==0));
+
+}
+
+
+TEST_CASE("Midel the GAME"){
+
+Player One("One"); //Creating player objects
+Player Two("Two");
+Game game(One, Two);
+for (int i=0;i<5;i++) {
+    game.playTurn();
+}
+
+CHECK((One.stacksize() + Two.stacksize ==52));
+
+}
+
+
+TEST_CASE("OFTER ONE YURN"){
+
+Player One("One"); //Creating player objects
+Player Two("Two");
+Game game(One, Two);
+game.playTurn();
+CHECK((One.stacksize() >26  && Two.stacksize <26) || (One.stacksize() <26 && Two.stacksize >26));
+}
 
