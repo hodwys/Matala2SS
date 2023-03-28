20591f7ba8df71706f086cc7e5fd9839182206a8
hodwys <hodwys2424@gmail.com>
2023-03-28 21:59:51 +0300

    fin

--- Test.cpp
+++ Test.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 
-TEST_CASE("befor START GAME"){
+TEST_CASE("Before the game started"){
     
 
     Player One("One"); //Creating player objects
@@ -28,7 +28,7 @@ TEST_CASE("befor START GAME"){
 
 
 
-TEST_CASE("in the START GAME"){
+TEST_CASE("At the beginning of the game"){
 
 Player One("One"); //Creating player objects
 Player Two("Two");
@@ -45,52 +45,7 @@ CHECK( con3);
 CHECK(con4);
 }
 
-
-
-TEST_CASE("END THE GAME"){
-
-Player One("One"); //Creating player objects
-Player Two("Two");
-Game game(One, Two);
-game.playAll();
-bool con1 = One.stacksize() == 0;
-bool con2 =  Two.stacksize()==0;
-bool con3 = (One.cardesTaken() + Two.cardesTaken()==52);
-CHECK(con3);
-CHECK(con1);
-CHECK(con2);
-
-CHECK_THROWS(game.playTurn());
-
-}
-
-
-TEST_CASE("Midel the GAME"){
-
-Player One("One"); 
-Player Two("Two");
-Game game(One, Two);
-for (int i=0;i<5;i++) {
-    game.playTurn();
-}
-
-bool con1 = (One.stacksize()<20);
-bool con2 = (Two.stacksize()<20);
-bool con3 = (One.stacksize() == Two.stacksize());
-bool con4 = ((One.cardesTaken()>0 || Two.cardesTaken()>0));
-
-CHECK(con4);
-CHECK(con1);
-CHECK(con2);
-CHECK(con3);
-CHECK((One.stacksize() + One.cardesTaken() + Two.stacksize() + Two.cardesTaken() ==52));
-
-
-
-}
-
-
-TEST_CASE("OFTER ONE TURN"){
+TEST_CASE("After one turn"){
 
 Player One("One"); //Creating player objects
 Player Two("Two");
@@ -119,16 +74,54 @@ else{
 }
 
 
+TEST_CASE("In the middle of the game"){
+
+Player One("One"); 
+Player Two("Two");
+Game game(One, Two);
+for (int i=0;i<5;i++) {
+    game.playTurn();
+}
+
+bool con1 = (One.stacksize()<20);
+bool con2 = (Two.stacksize()<20);
+bool con3 = (One.stacksize() == Two.stacksize());
+bool con4 = ((One.cardesTaken()>0 || Two.cardesTaken()>0));
+
+CHECK(con4);
+CHECK(con1);
+CHECK(con2);
+CHECK(con3);
+CHECK((One.stacksize() + One.cardesTaken() + Two.stacksize() + Two.cardesTaken() ==52));
+
+}
+
+
+TEST_CASE("At the end of the game"){
+
+Player One("One"); //Creating player objects
+Player Two("Two");
+Game game(One, Two);
+game.playAll();
+bool con1 = One.stacksize() == 0;
+bool con2 =  Two.stacksize()==0;
+bool con3 = (One.cardesTaken() + Two.cardesTaken()==52);
+CHECK(con3);
+CHECK(con1);
+CHECK(con2);
+
+CHECK_THROWS(game.playTurn());
+
+}
 
 
 
-TEST_CASE("function are error"){
+TEST_CASE("Throwing errors from functions"){
     
 
     Player One("One"); //Creating player objects
     Player Two("Two");
     Game game(One, Two);
-
 
 
     CHECK_NOTHROW(game.printLastTurn());
